56edfa2b5d76968b4804218beebe3d5966f00711
Mina Eskandar <dev.minaeskandar@gmail.com>
2025-12-12 15:42:01 -0300

    split challenges and leaderboard data

---
+++ challenges.js
@@ -0,0 +1,9 @@
+const challenges = [
+    {
+        "name": "Take a picture at your favourite study spot and put it <a href='https://drive.google.com/drive/folders/1T42ZKVzCZ8faeRbBtDU7en_19PGR54rd?usp=sharing'>here</a>",
+        "rules": "Take a clear photo at your favourite study spot. Be respectful of privacy and surroundings.",
+        "difficulty": "Easy",
+        "points": 10,
+        "bonus": "5 points for uniqueness and creativity"
+    }
+]
---
+++ leaderboard.js
@@ -0,0 +1,10 @@
+const leaderboardData ={
+    "Michelia": 10,
+    "Alexia": 10,
+    "Karla": 10,
+    "Maria Dumitrescu": 10,
+    "Rim": 10,
+    "Marta": 10,
+    "Daniel-Andrei Zgarta": 10,
+    "Michael Okolo":10
+}
