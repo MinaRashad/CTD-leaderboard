72b2334ebb06b2438f06123bb9de09614517a137
Mina Eskandar <dev.minaeskandar@gmail.com>
2026-01-28 11:25:52 -0300

    second challenge ready

--- challenges.js
+++ challenges.js
@@ -1,16 +1,18 @@
 const challenges = [
     {
-        "name": "Take a picture at your favourite study spot and put it <a href='https://drive.google.com/drive/folders/1T42ZKVzCZ8faeRbBtDU7en_19PGR54rd?usp=sharing'>here</a>",
-        "rules": "Take a clear photo at your favourite study spot. Be respectful of privacy and surroundings.",
-        "difficulty": "Easy",
-        "points": 10,
-        "bonus": "5 points for uniqueness and creativity"
+        name: "Favourite Study Spot",
+        rules: "Take a picture at your favourite study spot and upload it <a href='https://drive.google.com/drive/folders/1T42ZKVzCZ8faeRbBtDU7en_19PGR54rd?usp=sharing' target='_blank'>here</a>. Take a clear photo at your favourite study spot. Be respectful of privacy and surroundings.",
+        difficulty: "Easy",
+        points: 10,
+        bonus: "5 points for uniqueness and creativity",
+        expired: true
     },
     {
-        "name" : "Screenshot your coaching session and share it <a href='https://drive.google.com/drive/folders/1bhabZG_mQuc0uTqWRHuJQ0MqwFbK1wer?usp=sharing'>here</a>",
-        "rules": "Ensure the screenshot clearly shows the coaching session details. Blur out any sensitive information.",
-        "difficulty": "Easy",
-        "points": 15,
-        "bonus": "5 points for taking the session in an unusual location"
+        name: "Coaching Session",
+        rules: "Screenshot/selfie your coaching session and share it <a href='https://drive.google.com/drive/folders/1bhabZG_mQuc0uTqWRHuJQ0MqwFbK1wer?usp=sharing' target='_blank'>here</a>. Ask for permission first and blur out any sensitive information.",
+        difficulty: "Easy",
+        points: 15,
+        bonus: "5 points for taking the session in an unusual location",
+        expired: false
     }
 ]
